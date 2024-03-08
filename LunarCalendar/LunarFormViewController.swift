0e16bc09a4aea128814959dae7644b5bdf6cd05b
Sharon <sksblacklock@gmail.com>
2024-03-08 00:05:32 +0000

    commit

---
+++ LunarCalendar/LunarFormViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LunarFormViewController.swift
+//  LunarCalendar
+//
+//  Created by Default User on 3/7/24.
+//
+
+import UIKit
+
+class LunarFormViewController: UIViewController {
+
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    @IBOutlet weak var timeTextField: UITextField!
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
